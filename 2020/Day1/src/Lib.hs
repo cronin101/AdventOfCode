63e263f8a017e09284b5a6c731591122eec865cf
Aaron Cronin <cronin@include.cat>
2020-12-01 22:39:49 +0100

    IntSet Optimisation

--- 2020/Day1/src/Lib.hs
+++ 2020/Day1/src/Lib.hs
@@ -1,43 +1,42 @@
 module Lib
   ( pairs
-  , triplets'
   , loadInput
   , findPairWithSum
   , findTripletWithSum
   ) where
 
-import           Data.List                      ( tails
-                                                , nub
-                                                )
+import           Data.List                      ( tails )
+import           Data.Maybe                     ( listToMaybe )
 import           Data.Foldable                  ( find )
+import           Data.IntSet                    ( IntSet )
+import qualified Data.IntSet                   as IntSet
 
 -- Generate all unique pairs from a list input
-pairs :: Eq b => [b] -> [(b, b)]
-pairs xs = [ (x, y) | (x : ys) <- tails $ nub xs, y <- ys ]
+pairs :: IntSet -> [(Int, Int)]
+pairs intSet = [ (x, y) | (x : ys) <- tails $ IntSet.toList intSet, y <- ys ]
 
--- Generate all unique triplets from a list input
-triplets :: Eq c => [c] -> [(c, c, c)]
-triplets xs =
-  [ (x, y, z) | (x : ys) <- tails $ nub xs, (y : zs) <- tails ys, z <- zs ]
 
--- Optimised for the particular task, far fewer candidates generated
-triplets' :: (Eq c, Num c) => (c -> Bool) -> [c] -> [(c, c, c)]
-triplets' invariant xs =
+-- Naive: Generate all unique triplets from a list input
+triplets :: IntSet -> [(Int, Int, Int)]
+triplets intSet =
   [ (x, y, z)
-  | (x : ys) <- tails $ filter invariant $ nub xs
+  | (x : ys) <- tails $ IntSet.toList intSet
   , (y : zs) <- tails ys
-  , invariant $ x + y
-  , z <- zs
+  , z        <- zs
   ]
 
+-- Optimised: use look-up of the partial-sum to find the last element
+findTripletWithSum :: Int -> IntSet -> Maybe (Int, Int, Int)
+findTripletWithSum targetSum intSet = listToMaybe
+  [ (x, y, z)
+  | (x, y) <- pairs intSet
+  , let z = targetSum - (x + y)
+  , IntSet.member z intSet
+  ]
 
 findPairWithSum :: (Foldable t, Eq a, Num a) => a -> t (a, a) -> Maybe (a, a)
 findPairWithSum targetSum = find ((== targetSum) . (uncurry (+)))
 
-findTripletWithSum
-  :: (Foldable t, Eq a, Num a) => a -> t (a, a, a) -> Maybe (a, a, a)
-findTripletWithSum targetSum =
-  find ((== targetSum) . (\(x, y, z) -> x + y + z))
-
-loadInput :: String -> IO [Int]
-loadInput fileName = map read . lines <$> readFile ("src/" ++ fileName)
+loadInput :: String -> IO IntSet
+loadInput fileName =
+  IntSet.fromList . map read . lines <$> readFile ("src/" ++ fileName)
