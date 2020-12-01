fa72a1b284ccf7a6a541221c99902c31978f26e4
Aaron Cronin <cronin@include.cat>
2020-12-01 19:48:17 +0100

    Day1: Optimised case

--- 2020/Day1/app/Main.hs
+++ 2020/Day1/app/Main.hs
@@ -1,7 +1,7 @@
 module Main where
 
 import           Lib                            ( pairs
-                                                , triplets
+                                                , triplets'
                                                 , findPairWithSum
                                                 , findTripletWithSum
                                                 , loadInput
@@ -14,6 +14,6 @@ main = do
   let pairProduct = uncurry (*) <$> desiredPair
   print pairProduct
 
-  let desiredTriplet = findTripletWithSum 2020 $ triplets input
+  let desiredTriplet = findTripletWithSum 2020 $ triplets' (<= 2020) input
   let tripletProduct = fmap (\(x, y, z) -> x * y * z) desiredTriplet
   print tripletProduct
--- 2020/Day1/src/Lib.hs
+++ 2020/Day1/src/Lib.hs
@@ -1,6 +1,6 @@
 module Lib
   ( pairs
-  , triplets
+  , triplets'
   , loadInput
   , findPairWithSum
   , findTripletWithSum
@@ -12,13 +12,23 @@ import           Data.List                      ( tails
 import           Data.Foldable                  ( find )
 
 -- Generate all unique pairs from a list input
-pairs :: Eq a => [a] -> [(a, a)]
+pairs :: Eq b => [b] -> [(b, b)]
 pairs xs = [ (x, y) | (x : ys) <- tails $ nub xs, y <- ys ]
 
 -- Generate all unique triplets from a list input
 triplets :: Eq c => [c] -> [(c, c, c)]
 triplets xs =
   [ (x, y, z) | (x : ys) <- tails $ nub xs, (y : zs) <- tails ys, z <- zs ]
+
+-- Optimised for the particular task, far fewer candidates generated
+triplets' :: (Eq c, Num c) => (c -> Bool) -> [c] -> [(c, c, c)]
+triplets' invariant xs =
+  [ (x, y, z)
+  | (x : ys) <- tails $ filter invariant $ nub xs
+  , (y : zs) <- tails ys
+  , invariant $ x + y
+  , z <- zs
+  ]
 
 
 findPairWithSum :: (Foldable t, Eq a, Num a) => a -> t (a, a) -> Maybe (a, a)
