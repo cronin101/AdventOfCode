346b459b7f3c9f9fbf0469dae56541b62badf6fe
Aaron Cronin <cronin@include.cat>
2020-12-02 13:11:55 +0100

    Optimisation

--- 2020/Day2/src/Lib.hs
+++ 2020/Day2/src/Lib.hs
@@ -1,30 +1,14 @@
 module Lib
   ( loadInput
   , parseLine
-  , isValid1
-  , isValid2
   ) where
 
 import qualified Data.ByteString.Char8         as BSC
 import           Data.ByteString.Char8          ( ByteString )
-import           Data.IntSet                    ( IntSet )
-import qualified Data.IntSet                   as IntSet
-
-data ParsedLine = ParsedLine Int Int IntSet
-  deriving Show
-
-isValid1 :: ParsedLine -> Bool
-isValid1 (ParsedLine minimumcount maximumCount indices) =
-  charCount >= minimumcount && charCount <= maximumCount
-  where charCount = IntSet.size indices
-
-isValid2 :: ParsedLine -> Bool
-isValid2 (ParsedLine index1 index2 indices) =
-  IntSet.member (index1 - 1) indices /= IntSet.member (index2 - 1) indices
 
 -- Example line: "2-5 h: lcwghhkpkxvzkvrmxrv"
-parseLine :: ByteString -> ParsedLine
-parseLine bs = ParsedLine num1 num2 indices
+parseLine :: ByteString -> (Bool, Bool)
+parseLine bs = (testOneResult, testTwoResult)
  where
   -- remainder1: "-5 h: lcwghhkpkxvzkvrmxrv"
   Just (num1      , remainder1) = BSC.readInt bs
@@ -33,8 +17,20 @@ parseLine bs = ParsedLine num1 num2 indices
   -- remainder3: ": lcwghhkpkxvzkvrmxrv"
   Just (targetChar, remainder3) = BSC.uncons $ BSC.drop 1 remainder2
   passwordString                = BSC.drop 2 remainder3
-  indices =
-    IntSet.fromDistinctAscList $ BSC.elemIndices targetChar passwordString
+  charCount                     = BSC.count targetChar passwordString
+  testOneResult                 = charCount >= num1 && charCount <= num2
+  testTwoResult =
+    targetCharIsNthChar passwordString targetChar num1
+      /= targetCharIsNthChar passwordString targetChar num2
+
+readByteStringNthChar :: ByteString -> Int -> Maybe Char
+readByteStringNthChar byteString n
+  | n > BSC.length byteString = Nothing
+  | otherwise                 = Just $ BSC.index byteString $ n - 1
+
+targetCharIsNthChar :: ByteString -> Char -> Int -> Bool
+targetCharIsNthChar byteString targetChar n =
+  readByteStringNthChar byteString n == Just targetChar
 
 loadInput :: [Char] -> IO [ByteString]
 loadInput fileName = BSC.lines <$> BSC.readFile ("src/" ++ fileName)
