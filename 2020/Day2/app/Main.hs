ce7e3b71d531e481e457080b4350f7169d7007b7
Aaron Cronin <cronin@include.cat>
2020-12-02 13:11:55 +0100

    Optimisation

--- 2020/Day2/app/Main.hs
+++ 2020/Day2/app/Main.hs
@@ -2,14 +2,17 @@ module Main where
 
 import           Lib                            ( loadInput
                                                 , parseLine
-                                                , isValid1
-                                                , isValid2
                                                 )
+import           Data.ByteString                ( ByteString )
+
+answerOne :: [ByteString] -> Int
+answerOne input = length . filter fst $ map parseLine input
+
+answerTwo :: [ByteString] -> Int
+answerTwo input = length . filter snd $ map parseLine input
 
 main :: IO ()
 main = do
   input <- loadInput "input2.txt"
-  let count1 = length . filter isValid1 $ map parseLine input
-  print count1
-  let count2 = length . filter isValid2 $ map parseLine input
-  print count2
+  print $ answerOne input
+  print $ answerTwo input
