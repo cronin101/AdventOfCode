9c7710da480afc2d28b1ff3d70ce54181ae78524
Aaron Cronin <cronin@include.cat>
2020-12-11 19:33:57 +0100

    Part two complete

--- 2020/Day11/app/Main.hs
+++ 2020/Day11/app/Main.hs
@@ -3,9 +3,19 @@ module Main where
 import           Lib                            ( loadInput
                                                 , stableState
                                                 , passengerCount
+                                                , PassengerTolerance
+                                                  ( PassengerTolerance
+                                                  )
+                                                , AdjacencyType
+                                                  ( Immediate
+                                                  , NearestVisible
+                                                  )
                                                 )
 
 main :: IO ()
 main = do
-  emptyFerry <- loadInput "input11.txt"
-  print $ passengerCount $ stableState $ emptyFerry
+  firstFerry <- loadInput "input11.txt" (PassengerTolerance Immediate 3)
+  print $ passengerCount $ stableState firstFerry
+
+  secondFerry <- loadInput "input11.txt" (PassengerTolerance NearestVisible 4)
+  print $ passengerCount $ stableState secondFerry
