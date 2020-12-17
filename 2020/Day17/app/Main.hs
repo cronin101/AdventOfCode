6254570a15204e847186937b587cdf63655f4971
Aaron Cronin <cronin@include.cat>
2020-12-17 19:37:33 +0100

    Day 17 Complete

--- 2020/Day17/app/Main.hs
+++ 2020/Day17/app/Main.hs
@@ -3,10 +3,22 @@ module Main where
 import           Lib                            ( loadInput
                                                 , activeCount
                                                 , stepUntil
+                                                , Space
+                                                  ( ThreeDimensions
+                                                  , FourDimensions
+                                                  )
                                                 )
+import           Control.Arrow                  ( Arrow((***)) )
+import           Control.Monad                  ( join )
 
 main :: IO ()
 main = do
-  input <- loadInput "input17.txt"
-  let booted = stepUntil 6 input
-  print $ activeCount booted
+  let load d = loadInput d "input17.txt"
+  input3d <- load ThreeDimensions
+  input4d <- load FourDimensions
+
+  let (booted3d, booted4d) = join (***) (stepUntil 6) (input3d, input4d)
+
+  print $ activeCount booted3d
+  print $ activeCount booted4d
+
