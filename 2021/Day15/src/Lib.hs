470f767d79aa9d3e9eae4396805e432f4c008f60
Aaron Cronin <cronin@include.cat>
2021-12-16 01:12:21 +0100

    Cleanup

--- 2021/Day15/src/Lib.hs
+++ 2021/Day15/src/Lib.hs
@@ -53,33 +53,35 @@ loadInput fileName =
     <$> BSC.readFile
       ("src/" ++ fileName)
 
--- >>> floodFill <$> loadInput "example.txt"
+-- >>> solveDynamic <$> loadInput "example.txt"
 -- fromList [((0,0),Just 0),((0,1),Just 1),((0,2),Just 3),((0,3),Just 6),((0,4),Just 13),((0,5),Just 14),((0,6),Just 15),((0,7),Just 18),((0,8),Just 19),((0,9),Just 21),((1,0),Just 1),((1,1),Just 4),((1,2),Just 4),((1,3),Just 10),((1,4),Just 14),((1,5),Just 17),((1,6),Just 18),((1,7),Just 19),((1,8),Just 21),((1,9),Just 24),((2,0),Just 7),((2,1),Just 12),((2,2),Just 7),((2,3),Just 16),((2,4),Just 20),((2,5),Just 18),((2,6),Just 23),((2,7),Just 21),((2,8),Just 30),((2,9),Just 25),((3,0),Just 10),((3,1),Just 11),((3,2),Just 13),((3,3),Just 17),((3,4),Just 20),((3,5),Just 27),((3,6),Just 32),((3,7),Just 26),((3,8),Just 29),((3,9),Just 26),((4,0),Just 17),((4,1),Just 14),((4,2),Just 18),((4,3),Just 26),((4,4),Just 24),((4,5),Just 25),((4,6),Just 34),((4,7),Just 30),((4,8),Just 30),((4,9),Just 35),((5,0),Just 22),((5,1),Just 21),((5,2),Just 19),((5,3),Just 22),((5,4),Just 23),((5,5),Just 25),((5,6),Just 26),((5,7),Just 28),((5,8),Just 31),((5,9),Just 35),((6,0),Just 23),((6,1),Just 23),((6,2),Just 20),((6,3),Just 21),((6,4),Just 28),((6,5),Just 33),((6,6),Just 28),((6,7),Just 29),((6,8),Just 37),((6,9),Just 39),((7,0),Just 30),((7,1),Just 29),((7,2),Just 23),((7,3),Just 26),((7,4),Just 27),((7,5),Just 28),((7,6),Just 32),((7,7),Just 35),((7,8),Just 40),((7,9),Just 44),((8,0),Just 34),((8,1),Just 32),((8,2),Just 25),((8,3),Just 31),((8,4),Just 28),((8,5),Just 31),((8,6),Just 33),((8,7),Just 36),((8,8),Just 38),((8,9),Just 46),((9,0),Just 36),((9,1),Just 34),((9,2),Just 33),((9,3),Just 38),((9,4),Just 29),((9,5),Just 36),((9,6),Just 34),((9,7),Just 43),((9,8),Just 39),((9,9),Just 40)]
-floodFill :: Landscape -> SolveState
-floodFill landscape = floodRec initialState [(0, 0)]
+solveDynamic :: Landscape -> SolveState
+solveDynamic landscape = solve initialState [(0, 0)]
   where
-    adjacentMap = M.fromList $ map (\point -> (point, S.fromList $ adjacents landscape point)) $ M.keys landscape
-    initialState = M.insert (0, 0) (Just 0) $ M.map (const Nothing) landscape
-    floodRec :: SolveState -> [Coordinate] -> SolveState
-    floodRec state [] = state
-    floodRec state previouslyUpdated = floodRec (M.union (M.fromList mapUpdates) state) $ map fst mapUpdates
+    solve :: SolveState -> [Coordinate] -> SolveState
+    solve state [] = state
+    solve state previouslyUpdated = solve (M.union (M.fromList nextUpdates) state) $ map fst nextUpdates
       where
-        potentiallyRecalculated = S.toList $ S.unions $ map (fromJust . (`M.lookup` adjacentMap)) previouslyUpdated
-        mapUpdates = filter (\(point, newValue) -> newValue /= fromJust (M.lookup point state)) $ map (ap (,) newValue) potentiallyRecalculated
-        newValue point = case currentValue of
+        frontier = S.toList $ S.unions $ map adjacentsTo previouslyUpdated
+        nextUpdates = filter isDifferent $ map (ap (,) nextValue) frontier
+        isDifferent (point, nextVal) = nextVal /= currentValue point
+        adjacentsTo p = fromJust $ M.lookup p adjacentMap
+        currentValue p = fromJust $ M.lookup p state
+        nextValue point = case currentValue point of
           Just value -> Just $ minimum (value : options)
           _ -> Just $ minimum options
           where
             Just enterCost = M.lookup point landscape
-            options = map (+ enterCost) $ catMaybes $ S.toList $ S.map (fromJust . (`M.lookup` state)) $ fromJust $ M.lookup point adjacentMap
-            Just currentValue = M.lookup point state
+            options = map (+ enterCost) $ mapMaybe currentValue $ S.toList $ adjacentsTo point
+    adjacentMap = M.fromList $ map (\point -> (point, S.fromList $ adjacents landscape point)) $ M.keys landscape
+    initialState = M.insert (0, 0) (Just 0) $ M.map (const Nothing) landscape
 
 -- >>> exitCost <$> loadInput "example.txt"
 -- 40
 exitCost :: Landscape -> Int
 exitCost landscape = fromJust $ fromJust $ M.lookup (maxX, maxY) solved
   where
-    solved = floodFill landscape
+    solved = solveDynamic landscape
     maxX = maximum $ map fst $ M.keys solved
     maxY = maximum $ map snd $ M.keys solved
 
