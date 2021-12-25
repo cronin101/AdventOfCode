30d97c17894af873a51bd94be3ba5474da77d90a
Aaron Cronin <acronin@microsoft.com>
2021-12-25 19:42:56 +0100

    2021 Day 22 Complete

--- 2021/Day22/src/Lib.hs
+++ 2021/Day22/src/Lib.hs
@@ -3,15 +3,12 @@
 
 module Lib (virtualVolume, virtualSimulate, loadInput) where
 
-import Control.Applicative ((<|>))
+import Control.Applicative (Alternative (empty), (<|>))
 import qualified Data.Attoparsec.ByteString.Char8 as A
-import Data.Bifunctor (bimap)
 import qualified Data.ByteString.Char8 as BSC
 import Data.Either (fromRight)
-import qualified Data.IntMap.Strict as IM
 import Data.List (intercalate)
-import qualified Data.Map as M
-import Data.Maybe (fromMaybe, mapMaybe)
+import Data.Maybe (catMaybes, fromJust, fromMaybe, isJust, isNothing, mapMaybe)
 import qualified Data.Set as S
 
 type Coord3D a = (a, a, a)
@@ -28,26 +25,19 @@ type VirtualReactor a = S.Set (Cuboid a)
 -- Right (True,((10,10,10),(12,12,12)))
 parseInstruction :: Integral a => A.Parser (Instruction a)
 parseInstruction = do
-  toggle <- "on" <|> "off"
-  " x="
-  xmin <- A.signed A.decimal
-  ".."
-  xmax <- A.signed A.decimal
-  ",y="
-  ymin <- A.signed A.decimal
-  ".."
-  ymax <- A.signed A.decimal
-  ",z="
-  zmin <- A.signed A.decimal
-  ".."
-  zmax <- A.signed A.decimal
+  toggle <- ("on" <|> "off") <* " "
+  xmin <- "x=" *> A.signed A.decimal
+  xmax <- ".." *> A.signed A.decimal
+  ymin <- ",y=" *> A.signed A.decimal
+  ymax <- ".." *> A.signed A.decimal
+  zmin <- ",z=" *> A.signed A.decimal
+  zmax <- ".." *> A.signed A.decimal
   return (toggle == "on", ((xmin, ymin, zmin), (xmax, ymax, zmax)))
 
-overlap :: Integral a => Cuboid a -> Cuboid a -> Cuboid a
-overlap ((xmin, ymin, zmin), (xmax, ymax, zmax)) ((xmin', ymin', zmin'), (xmax', ymax', zmax')) = ((max xmin xmin', max ymin ymin', max zmin zmin'), (min xmax xmax', min ymax ymax', min zmax zmax'))
-
-union :: Integral a => Cuboid a -> Cuboid a -> [Cuboid a]
-union a b = [overlap a b]
+intersection :: Integral a => Cuboid a -> Cuboid a -> Maybe (Cuboid a)
+intersection ((xmin, ymin, zmin), (xmax, ymax, zmax)) ((xmin', ymin', zmin'), (xmax', ymax', zmax')) = if ox <= ox' && oy <= oy' && oz <= oz' then Just overlap else Nothing
+  where
+    overlap@((ox, oy, oz), (ox', oy', oz')) = ((max xmin xmin', max ymin ymin', max zmin zmin'), (min xmax xmax', min ymax ymax', min zmax zmax'))
 
 volume :: Integral a => Cuboid a -> a
 volume ((xmin, ymin, zmin), (xmax, ymax, zmax)) = (abs (xmax - xmin) + 1) * (abs (ymax - ymin) + 1) * (abs (zmax - zmin) + 1)
@@ -64,211 +54,63 @@ loadInput fileName =
     <$> BSC.readFile
       ("src/" ++ fileName)
 
-boundedPoints :: Integral a => Cuboid a -> [Coord3D a]
-boundedPoints cuboid = [(x, y, z) | x <- [xmin .. xmax], y <- [ymin .. ymax], z <- [zmin .. zmax]]
+boundedPoints :: Integral a => Cuboid a -> S.Set (Coord3D a)
+boundedPoints cuboid =
+  if doesIntersect cuboid bounds
+    then S.fromList [(x, y, z) | x <- [xmin .. xmax], y <- [ymin .. ymax], z <- [zmin .. zmax]]
+    else S.empty
   where
-    ((xmin, ymin, zmin), (xmax, ymax, zmax)) = overlap cuboid ((-50, -50, -50), (50, 50, 50))
+    bounds = ((-50, -50, -50), (50, 50, 50))
+    Just ((xmin, ymin, zmin), (xmax, ymax, zmax)) = intersection cuboid bounds
 
 doesIntersect :: Integral a => Cuboid a -> Cuboid a -> Bool
-doesIntersect a b = (> 0) . volume $ overlap a b
+doesIntersect a b = isJust $ intersection a b
 
 physicalEvaluate :: Integral a => PhysicalReactor a -> Instruction a -> PhysicalReactor a
-physicalEvaluate r (True, cuboid) = S.union (S.fromList $ boundedPoints cuboid) r
-physicalEvaluate r (False, cuboid) = r S.\\ S.fromList (boundedPoints cuboid)
+physicalEvaluate r (True, cuboid) = S.union (boundedPoints cuboid) r
+physicalEvaluate r (False, cuboid) = r S.\\ boundedPoints cuboid
 
 physicalSimulate :: Integral a => [Instruction a] -> PhysicalReactor a
 physicalSimulate = foldl physicalEvaluate S.empty
 
--- >>> S.size . physicalSimulate <$> loadInput "smallExample.txt"
--- 39
-
--- >>> S.size . physicalSimulate <$> loadInput "largerExample.txt"
--- 590784
-
--- >>> S.size . physicalSimulate <$> loadInput "input.txt"
--- 553201
-
-normalizedReactor :: Integral a => VirtualReactor a -> Cuboid a -> VirtualReactor a
-normalizedReactor r c = S.unions $ S.map (\c' -> S.fromList $ snd $ normalize (c, c')) r
-
-normalizedCuboid :: Integral a => VirtualReactor a -> Cuboid a -> S.Set (Cuboid a)
-normalizedCuboid r c = S.unions $ S.map (\c' -> S.fromList $ fst $ normalize (c, c')) r
-
 -- >>> ((virtualEvaluate S.empty) . head)  <$> loadInput "smallExample.txt"
 -- fromList [((10,10,10),(12,12,12))]
 virtualEvaluate :: Integral a => VirtualReactor a -> Instruction a -> VirtualReactor a
-virtualEvaluate r (True, c)
-  | S.null r = S.singleton c
-  | otherwise = S.union (normalizedCuboid r c) (normalizedReactor r c)
+virtualEvaluate r (True, inserted)
+  | S.null r = S.singleton inserted
+  | otherwise = S.union (S.singleton inserted) $ S.unions $ S.map leftover r
+  where
+    leftover existing = case intersection inserted existing of
+      Just duplicate -> existing `subtractCuboid` duplicate
+      Nothing -> S.singleton existing
 virtualEvaluate r (False, c)
   | S.null r = S.empty
-  | otherwise = S.difference (normalizedReactor r c) (normalizedCuboid r c)
+  | otherwise = S.unions $ S.map (`subtractCuboid` c) r
 
--- >>> (virtualVolume. virtualSimulate . take 2 ) <$> loadInput "smallExample.txt"
--- 46
 virtualSimulate :: Integral a => [Instruction a] -> VirtualReactor a
 virtualSimulate = foldl virtualEvaluate S.empty
-
--- >>> ( S.toList . virtualSimulate . take 3) <$> loadInput "smallExample.txt"
--- [((10,10,12),(10,10,12)),((10,11,12),(10,11,12)),((10,12,10),(10,12,10)),((10,12,11),(10,12,11)),((10,12,12),(10,12,12)),((11,10,12),(11,10,12)),((11,11,12),(11,11,12)),((11,11,13),(11,11,13)),((11,12,10),(11,12,10)),((11,12,11),(11,12,11)),((11,12,12),(11,12,12)),((11,12,13),(11,12,13)),((11,13,11),(11,13,11)),((11,13,12),(11,13,12)),((11,13,13),(11,13,13)),((12,10,10),(12,10,10)),((12,10,11),(12,10,11)),((12,10,12),(12,10,12)),((12,11,10),(12,11,10)),((12,11,11),(12,11,11)),((12,11,12),(12,11,12)),((12,11,13),(12,11,13)),((12,12,10),(12,12,10)),((12,12,11),(12,12,11)),((12,12,12),(12,12,12)),((12,12,13),(12,12,13)),((12,13,11),(12,13,11)),((12,13,12),(12,13,12)),((12,13,13),(12,13,13)),((13,11,11),(13,11,11)),((13,11,12),(13,11,12)),((13,11,13),(13,11,13)),((13,12,11),(13,12,11)),((13,12,12),(13,12,12)),((13,12,13),(13,12,13)),((13,13,11),(13,13,11)),((13,13,12),(13,13,12)),((13,13,13),(13,13,13))]
-
--- >>> (virtualVolume. virtualSimulate) <$> loadInput "virtualExample.txt"
--- 39
 
 virtualVolume :: Integral a => VirtualReactor a -> a
 virtualVolume r = sum $ map volume $ S.toList r
 
--- >>> normalize (((10,10,12),(10,11,12)), ((10,10,10),(10,10,10)))
--- ([((10,11,12),(10,11,12)),((10,10,12),(10,10,12))],[((10,10,10),(10,10,10))])
-
-simplify :: Integral a => [Cuboid a] -> [Cuboid a]
-simplify [] = []
-simplify c =
-  if volume bounding == sum (map volume c)
-    then [bounding]
-    else c
-  where
-    bounding = ((xmin, ymin, zmin), (xmax, ymax, zmax))
-    xmin = minimum $ map ((\(x, y, z) -> x) . fst) c
-    ymin = minimum $ map ((\(x, y, z) -> y) . fst) c
-    zmin = minimum $ map ((\(x, y, z) -> z) . fst) c
-    xmax = maximum $ map ((\(x, y, z) -> x) . fst) c
-    ymax = maximum $ map ((\(x, y, z) -> y) . fst) c
-    zmax = maximum $ map ((\(x, y, z) -> z) . fst) c
-
--- >>> normalize (((10,10,10),(12,12,12)), ((11,11,11),(13,13,13)))
--- ([((11,11,11),(12,12,12)),((10,11,10),(10,12,10)),((10,10,10),(10,10,10)),((10,11,11),(10,12,12)),((10,10,11),(10,10,12)),((11,10,11),(12,10,12)),((11,10,10),(12,10,10)),((11,11,10),(12,12,10))],[((11,11,11),(12,12,12)),((13,11,11),(13,13,13)),((11,13,11),(12,13,12)),((11,13,13),(12,13,13)),((11,11,13),(12,12,13))])
-normalize :: Integral a => (Cuboid a, Cuboid a) -> ([Cuboid a], [Cuboid a])
-normalize (a, b) = case valid [overlap a b] of
-  [overlap] -> (valid $ o : a_rest, valid $ o : b_rest)
-  _ -> ([a], [b])
-  where
-    valid cuboids = filter (\((x, y, z), (x', y', z')) -> x <= x' && y <= y' && z <= z') cuboids
-    o = overlap a b
-    a_rest = rest a o
-    b_rest = rest b o
-    rest c@((xmin, ymin, zmin), (xmax, ymax, zmax)) o@((xmin', ymin', zmin'), (xmax', ymax', zmax')) =
-      leftSide ++ rightSide ++ top ++ bottom ++ front ++ back
-      where
-        leftMin = xmin
-        leftMax = xmin' - 1
-        bottomMin = ymin
-        bottomMax = ymin' - 1
-        topMin = ymax' + 1
-        topMax = ymax
-        frontMin = zmin
-        frontMax = zmin' - 1
-        backMax = zmax
-        backMin = zmax' + 1
-        leftSideCenter =
-          simplify $
-            valid
-              [ -- left center top
-                overlap c ((leftMin, topMin, zmin'), (leftMax, topMax, zmax')),
-                -- left center mid
-                overlap c ((leftMin, ymin', zmin'), (leftMax, ymax', zmax')),
-                -- left center bottom
-                overlap c ((leftMin, bottomMin, zmin'), (leftMax, bottomMax, zmax'))
-              ]
-        leftSideFront =
-          simplify $
-            valid
-              [ -- left front top
-                overlap c ((leftMin, topMin, frontMin), (leftMax, topMax, frontMax)),
-                -- left front mid
-                overlap c ((leftMin, ymin', frontMin), (leftMax, ymax', frontMax)),
-                -- left front bottom
-                overlap c ((leftMin, bottomMin, frontMin), (leftMax, bottomMax, frontMax))
-              ]
-        leftSideBack =
-          simplify $
-            valid
-              [ -- left back top
-                overlap c ((leftMin, topMin, backMin), (leftMax, topMax, backMax)),
-                -- left back mid
-                overlap c ((leftMin, ymin', backMin), (leftMax, ymax', backMax)),
-                -- left back bottom
-                overlap c ((leftMin, backMin, frontMin), (leftMax, bottomMax, backMax))
-              ]
-        leftSide = simplify $ leftSideFront ++ leftSideCenter ++ leftSideBack
-        rightMin = xmax' + 1
-        rightMax = xmax
-        rightSideCenter =
-          simplify $
-            valid
-              [ -- right center top
-                overlap c ((rightMin, topMin, zmin'), (rightMax, topMax, zmax')),
-                -- right center mid
-                overlap c ((rightMin, ymin', zmin'), (rightMax, ymax', zmax')),
-                -- right center bottom
-                overlap c ((rightMin, bottomMin, zmin'), (rightMax, bottomMax, zmax'))
-              ]
-        rightSideFront =
-          simplify $
-            valid
-              [ -- right front top
-                overlap c ((rightMin, topMin, frontMin), (rightMax, topMax, frontMax)),
-                -- right front mid
-                overlap c ((rightMin, ymin', frontMin), (rightMax, ymax', frontMax)),
-                -- right front bottom
-                overlap c ((rightMin, bottomMin, frontMin), (rightMax, bottomMax, frontMax))
-              ]
-        rightSideBack =
-          simplify $
-            valid
-              [ -- right back top
-                overlap c ((rightMin, topMin, backMin), (rightMax, topMax, backMax)),
-                -- right back mid
-                overlap c ((rightMin, ymin', backMin), (rightMax, ymax', backMax)),
-                -- right back bottom
-                overlap c ((rightMin, backMin, frontMin), (rightMax, bottomMax, backMax))
-              ]
-        rightSide = simplify $ rightSideFront ++ rightSideCenter ++ rightSideBack
-        middleMin = xmin'
-        middleMax = xmax'
-        top =
-          simplify $
-            valid
-              [ -- middle center top
-                overlap c ((middleMin, topMin, zmin'), (middleMax, topMax, zmax')),
-                -- middle front top
-                overlap c ((middleMin, topMin, frontMin), (middleMax, topMax, frontMax)),
-                -- middle back top
-                overlap c ((middleMin, topMin, backMin), (middleMax, topMax, backMax))
-              ]
-        bottom =
-          simplify $
-            valid
-              [ ---middle center bottom
-                overlap c ((middleMin, bottomMin, zmin'), (middleMax, bottomMax, zmax')),
-                -- middle front bottom
-                overlap c ((middleMin, bottomMin, frontMin), (middleMax, bottomMax, frontMax)),
-                -- middle back bottom
-                overlap c ((middleMin, bottomMin, backMin), (middleMax, bottomMax, backMax))
-              ]
-        front =
-          [ --- middle front mid
-            overlap c ((middleMin, ymin', frontMin), (middleMax, ymax', frontMax))
-          ]
-        back =
-          [ -- middle back min
-            overlap c ((middleMin, ymin', backMin), (middleMax, ymax', backMax))
-          ]
-
--- >>> bimap volume volume $ (((10,10,10),(12,12,12)), ((11,11,11),(13,13,13)))
--- (27,27)
-
--- >>> bimap (map volume) (map volume) $ normalize $ (((10,10,10),(12,12,12)), ((11,11,11),(13,13,13)))
--- ([8,2,1,4,2,4,2,4],[8,9,4,2,4])
-
--- >>> bimap (sum . map volume) (sum . map volume) $ normalize $ (((10,10,10),(12,12,12)), ((11,11,11),(13,13,13)))
--- (27,27)
-
--- >>> S.difference (S.fromList $ boundedPoints ((10,10,10),(12,12,12))) (S.fromList $ concatMap boundedPoints $ fst $ normalize $ (((10,10,10),(12,12,12)), ((11,11,11),(13,13,13))))
--- fromList []
-
--- >>> S.difference  (S.fromList $ concatMap boundedPoints $ fst $ normalize $ (((10,10,10),(12,12,12)), ((11,11,11),(13,13,13)))) (S.fromList $ boundedPoints ((10,10,10),(12,12,12)))
--- fromList []
-
--- >>> S.difference (S.fromList $ boundedPoints ((11,11,11),(13,13,13))) (S.fromList $ concatMap boundedPoints $ snd $ normalize $ (((10,10,10),(12,12,12)), ((11,11,11),(13,13,13))))
--- fromList []
+-- >>> subtractCuboid ((10,10,10),(12,12,12)) ((11,11,11),(13,13,13))
+-- fromList [((10,10,10),(10,10,10)),((10,10,11),(10,10,12)),((10,11,10),(10,12,10)),((10,11,11),(10,12,12)),((11,10,10),(12,10,10)),((11,10,11),(12,10,12)),((11,11,10),(12,12,10))]
+subtractCuboid :: Integral a => Cuboid a -> Cuboid a -> S.Set (Cuboid a)
+subtractCuboid a b = case intersection a b of
+  Nothing -> S.singleton a
+  Just overlap
+    | overlap == a -> S.empty
+    | otherwise -> S.delete overlap (S.fromList $ parts a overlap)
+    where
+      parts c@((xmin, ymin, zmin), (xmax, ymax, zmax)) o@((xmin', ymin', zmin'), (xmax', ymax', zmax')) =
+        catMaybes ([front, middleForFrontBack, back] <*> catMaybes ([top, middleForTopBottom, bottom] <*> catMaybes ([left, middleForLeftRight, right] <*> [c])))
+        where
+          left = (`intersection` ((xmin, ymin, zmin), (xmin' - 1, ymax, zmax)))
+          middleForLeftRight = (`intersection` ((xmin', ymin, zmin), (xmax', ymax, zmax)))
+          right = (`intersection` ((xmax' + 1, ymin, zmin), (xmax, ymax, zmax)))
+          top = (`intersection` ((xmin, ymax' + 1, zmin), (xmax, ymax, zmax)))
+          middleForTopBottom = (`intersection` ((xmin, ymin', zmin), (xmax, ymax', zmax)))
+          bottom = (`intersection` ((xmin, ymin, zmin), (xmax, ymin' - 1, zmax)))
+          front = (`intersection` ((xmin, ymin, zmax' + 1), (xmax, ymax, zmax)))
+          middleForFrontBack = (`intersection` ((xmin, ymin, zmin'), (xmax, ymax, zmax')))
+          back = (`intersection` ((xmin, ymin, zmin), (xmax, ymax, zmin' - 1)))
