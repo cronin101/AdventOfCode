8efa78880cdf8db8848a84b58c665bf742068ef9
Aaron Cronin <acronin@Aarons-M1.local>
2022-12-26 17:24:53 +0100

    Day 16 part 2 complete

--- 2022/Day16/app/Main.hs
+++ 2022/Day16/app/Main.hs
@@ -1,8 +1,9 @@
 module Main (main) where
 
-import Lib (loadInput, solutionA)
+import Lib (loadInput, solutionA, solutionB)
 
 main :: IO ()
 main = do
   input <- loadInput "input.txt"
   print $ solutionA input
+  print $ solutionB input
