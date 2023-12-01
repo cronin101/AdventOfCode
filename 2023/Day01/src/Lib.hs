f5d19ae3589837050ddac548ba1e0ad940a783ed
Aaron Cronin <acronin@Aarons-M1.local>
2023-12-02 00:57:37 +0100

    Parser refactor

--- 2023/Day01/src/Lib.hs
+++ 2023/Day01/src/Lib.hs
@@ -8,73 +8,42 @@ where
 
 import Data.Attoparsec.ByteString.Char8 (letter_ascii)
 import Data.Attoparsec.ByteString.Char8 qualified as A
+import Data.Attoparsec.Combinator qualified as A
 import Data.ByteString.Char8 qualified as BSC
 import Data.Either (fromRight)
 import Data.Functor (($>))
 import Data.Maybe (catMaybes)
 
--- A hacky parser that will Parse () if there is a match but leave the character in the stream
--- >>> A.parseOnly (parseIfPeekMatches 'a') "abc"
--- Right ()
-parseIfPeekMatches :: Char -> A.Parser ()
-parseIfPeekMatches match = do
-  char <- A.peekChar
-  case char of
-    Just c -> if c == match then return () else fail "Not a match"
-    Nothing -> fail "End of input"
+-- >>> A.parse (stringWithRewindLast "eight") "eightwo"
+-- Done "two" "eight"
+stringWithRewindLast :: BSC.ByteString -> A.Parser BSC.ByteString
+stringWithRewindLast bs = A.string (BSC.init bs) <* A.lookAhead (A.char $ BSC.last bs) $> bs
 
--- Used to match eightwone, etc. Leaves the first digit of the next number in the stream
--- >>> A.parse (stringWithPeekLast "eight") "eightwone"
--- Done "twone" "eight"
-stringWithPeekLast :: BSC.ByteString -> A.Parser BSC.ByteString
-stringWithPeekLast bs = A.string (BSC.init bs) *> parseIfPeekMatches (BSC.last bs) $> bs
+spelledDigits :: [A.Parser Char]
+spelledDigits =
+  zipWith
+    (\str val -> stringWithRewindLast str $> val)
+    ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+    ['1' .. '9']
 
--- Does what it says on the tin
--- >>> A.parseOnly (digitParser True) "1"
--- Right '1'
--- >>> A.parseOnly (digitParser False) "one"
--- Left "Failed reading: empty"
--- >>> A.parseOnly (digitParser True) "one"
--- Right '1'
 digitParser :: Bool -> A.Parser Char
-digitParser acceptSpelledDigits =
-  A.choice
-    ( A.digit
-        : ( if acceptSpelledDigits
-              then
-                [ stringWithPeekLast "one" $> '1',
-                  stringWithPeekLast "two" $> '2',
-                  stringWithPeekLast "three" $> '3',
-                  stringWithPeekLast "four" $> '4',
-                  stringWithPeekLast "five" $> '5',
-                  stringWithPeekLast "six" $> '6',
-                  stringWithPeekLast "seven" $> '7',
-                  stringWithPeekLast "eight" $> '8',
-                  stringWithPeekLast "nine" $> '9'
-                ]
-              else []
-          )
-    )
+digitParser acceptSpelledDigits = A.choice (A.digit : [spelledDigit | acceptSpelledDigits, spelledDigit <- spelledDigits])
 
 -- Progresses through the string to find matching digits, throwing away a character for each failure
 -- >>> A.parseOnly (parseFirstAndLastDigit True) "eightwone"
 -- Right 81
 parseFirstAndLastDigit :: Bool -> A.Parser Int
 parseFirstAndLastDigit acceptSpelledDigits = do
-  numbers <-
-    catMaybes
-      <$> A.many1
-        ( A.choice
-            [ Just <$> digitParser acceptSpelledDigits,
-              letter_ascii $> Nothing
-            ]
-        )
+  numbers <- catMaybes <$> A.many1 (A.choice [Just <$> digitParser acceptSpelledDigits, letter_ascii $> Nothing])
   let twoDigitNumber = [head numbers, last numbers]
   return $ read twoDigitNumber
+
+parseInput :: Bool -> A.Parser [Int]
+parseInput acceptSpelledDigits = A.sepBy1 (parseFirstAndLastDigit acceptSpelledDigits) A.endOfLine
 
 -- >>> loadInput False "example.txt"
 -- [12,38,15,77]
 -- >>> loadInput True "example2.txt"
 -- [29,82,23,34,42,24,77]
 loadInput :: Bool -> [Char] -> IO [Int]
-loadInput acceptSpelledDigits = (map (fromRight 0 . A.parseOnly (parseFirstAndLastDigit acceptSpelledDigits)) . BSC.lines <$>) . BSC.readFile . ("src/" ++)
+loadInput acceptSpelledDigits = (fromRight [] . A.parseOnly (parseInput acceptSpelledDigits) <$>) . BSC.readFile . ("src/" ++)
