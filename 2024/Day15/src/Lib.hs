fc78f41cb55d8ba685af4b5c8ec91f87023a6220
Aaron Cronin <acronin@MacBookPro.localdomain>
2024-12-15 21:17:02 +0100

    Copilot comments

--- 2024/Day15/src/Lib.hs
+++ 2024/Day15/src/Lib.hs
@@ -20,6 +20,7 @@ import Data.Map qualified as M
 import Data.Maybe (catMaybes, fromJust, isJust)
 import Data.Set qualified as S
 
+-- Parses a tile from the warehouse map.
 parseTile :: A.Parser Tile
 parseTile =
   A.choice
@@ -28,6 +29,7 @@ parseTile =
       Robot <$ "@"
     ]
 
+-- Parses a direction from the movement instructions.
 parseDirection :: A.Parser Direction
 parseDirection =
   A.choice
@@ -37,29 +39,35 @@ parseDirection =
       R <$ ">"
     ]
 
+-- Parses a row of the warehouse map, identifying the coordinates of each tile.
 -- >>> A.parseOnly parseRow "#..O..O.O#"
 -- Right [((0,0),#),((3,0),O),((6,0),O),((8,0),O),((9,0),#)]
 parseRow :: A.Parser [(Coord2D, Tile)]
 parseRow = map (second fromJust) . filter (isJust . snd) . zipWith (\x -> ((x, 0),)) [0 ..] <$> A.many1 (Just <$> parseTile <|> Nothing <$ ".")
 
+-- Parses the entire warehouse map, row by row.
 parseRoom :: A.Parser Room
 parseRoom = Room . M.unions . zipWith (\y -> M.fromList . map (first ((,y) . fst))) [0 ..] . reverse <$> (parseRow `A.sepBy1` A.endOfLine)
 
+-- Parses the initial state by parsing the room and the list of directions.
 parseState :: A.Parser State
 parseState = do
   room <- parseRoom <* A.count 2 A.endOfLine
   directions <- catMaybes <$> A.many1 (Just <$> parseDirection <|> Nothing <$ A.endOfLine)
   return $ State room (head . M.keys . M.filter (== Robot) $ tiles' room) directions
 
+-- Converts a direction into a vector for movement.
 toVector :: Direction -> Coord2D
 toVector U = (0, 1)
 toVector D = (0, -1)
 toVector L = (-1, 0)
 toVector R = (1, 0)
 
+-- Calculates the new position by adding vectors.
 add :: Coord2D -> Coord2D -> Coord2D
 add (x1, y1) (x2, y2) = (x1 + x2, y1 + y2)
 
+-- Predicts the next collisions based on the current direction and position.
 nextCollisions :: Room -> Direction -> (Coord2D, Tile) -> S.Set (Coord2D, Tile)
 nextCollisions (Room tiles) direction (actor, _) =
   let next = add (toVector direction) actor
@@ -74,36 +82,25 @@ nextCollisions (Room tiles) direction (actor, _) =
         (c, Just t) -> S.singleton (c, t)
         _ -> S.empty
 
+-- Calculates all potential collisions for a given direction.
 allCollisions :: Room -> Direction -> Coord2D -> S.Set (Coord2D, Tile)
 allCollisions room direction robot =
   let propagate = S.unions . S.map (nextCollisions room direction)
    in S.unions $ takeWhile (not . S.null) $ iterate propagate (S.singleton (robot, Robot))
 
+-- Executes a single step based on the current state and direction.
 step :: State -> State
-step s@(State _ _ []) = s
+step s@(State _ _ []) = s -- No more directions
 step s@(State room@(Room tiles) robot (d : ds))
-  | S.member Wall $ S.map snd collisions = s {directions' = ds}
+  | S.member Wall $ S.map snd collisions = s {directions' = ds} -- Hit a wall
   | otherwise = s {room' = room'', robot' = robot'', directions' = ds}
   where
     move = add (toVector d)
     robot'' = move robot
-    collisions = allCollisions room d robot
-    unmoved = tiles M.\\ M.fromList (S.toList collisions)
-    moved = M.fromList (map (first move) $ S.toList collisions)
+    collisions = allCollisions room d robot -- All collisions in the direction of movement
+    unmoved = tiles M.\\ M.fromList (S.toList collisions) -- Tiles that are not moved
+    moved = M.fromList (map (first move) $ S.toList collisions) -- Tiles that are moved
     room'' = Room $ M.union moved unmoved
-
-solve :: State -> State
-solve = until (null . directions') step
-
-gpsCoordinate :: Room -> Coord2D -> Int
-gpsCoordinate (Room tiles) (x, y) = (x - minX) + (100 * (maxY - y))
-  where
-    coords = M.keys tiles
-    minX = minimum . map fst $ coords
-    maxY = maximum . map snd $ coords
-
-coordinateSum :: Room -> Int
-coordinateSum r = sum $ map (gpsCoordinate r) $ M.keys $ M.filter (\case Box -> True; LeftBox -> True; _ -> False) $ tiles' r
 
 -- >>> solve <$> loadInput "example.txt"
 -- State {room' =
@@ -117,12 +114,28 @@ coordinateSum r = sum $ map (gpsCoordinate r) $ M.keys $ M.filter (\case Box -> 
 -- #O.....OO#
 -- #OO....OO#
 -- ##########, robot' = (3,5), directions' = []}
+solve :: State -> State
+solve = until (null . directions') step
 
+-- Calculates the GPS coordinate of a box based on its position in the room.
+gpsCoordinate :: Room -> Coord2D -> Int
+gpsCoordinate (Room tiles) (x, y) = (x - minX) + (100 * (maxY - y))
+  where
+    coords = M.keys tiles
+    minX = minimum . map fst $ coords
+    maxY = maximum . map snd $ coords
+
+-- Calculates the sum of all GPS coordinates for the boxes in the room.
+coordinateSum :: Room -> Int
+coordinateSum r = sum $ map (gpsCoordinate r) $ M.keys $ M.filter (\case Box -> True; LeftBox -> True; _ -> False) $ tiles' r
+
+-- Solves the puzzle for part 1 by calculating the sum of GPS coordinates after executing all moves.
 -- >>> part1 <$> loadInput "example.txt"
 -- 10092
 part1 :: State -> Int
 part1 = coordinateSum . room' . solve
 
+-- Loads the initial state from a file.
 -- >>> loadInput "example.txt"
 -- State {room' =
 -- ##########
@@ -138,6 +151,7 @@ part1 = coordinateSum . room' . solve
 loadInput :: [Char] -> IO State
 loadInput = (fromRight (State (Room M.empty) (0, 0) []) . A.parseOnly parseState <$>) . BSC.readFile . ("src/" ++)
 
+-- Expands the room to twice its width, adjusting the tiles accordingly.
 -- >>>  expand <$> loadInput "example.txt"
 -- State {room' =
 -- ####################
@@ -159,6 +173,7 @@ expand (State (Room tiles) robot d) = State (Room tiles'') (shift robot) d
         concatMap (\case (c@(x, y), Box) -> [(shift c, LeftBox), ((2 * x, y), RightBox)]; (c@(x, y), Wall) -> [(shift c, Wall), ((2 * x, y), Wall)]; (c, t) -> [(shift c, t)]) $
           M.toList tiles
 
+-- Solves the puzzle for part 2 by expanding the room and then calculating the sum of GPS coordinates after executing all moves.
 -- >>> solve . expand <$> loadInput "example.txt"
 -- State {room' =
 -- ####################
@@ -171,8 +186,6 @@ expand (State (Room tiles) robot d) = State (Room tiles'') (shift robot) d
 -- ##..@......[].[][]##
 -- ##......[][]..[]..##
 -- ####################, robot' = (3,2), directions' = []}
-
 -- >>> part2 <$> loadInput "example.txt"
--- 9021
 part2 :: State -> Int
 part2 = coordinateSum . room' . solve . expand
