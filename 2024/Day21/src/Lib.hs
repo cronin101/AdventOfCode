053e38479dd95e2c0a5fe980d0d7003aa477ad7b
Aaron Cronin <acronin@MacBookPro.localdomain>
2024-12-22 20:54:53 +0100

    Copilit comments

--- 2024/Day21/src/Lib.hs
+++ 2024/Day21/src/Lib.hs
@@ -24,18 +24,20 @@ data NumPad = Num Int | Submit deriving (Show, Eq, Ord)
 
 data Key = LeftKey | RightKey | UpKey | DownKey | EnterKey deriving (Show, Eq, Ord)
 
--- (Numpad, Robots)
+-- The position state of the robot's arm on the numeric keypad and the sequence of keys pressed
 type PositionState = (NumPad, [Key])
 
--- (Output (rev), PositionState)
+-- The state of the robot's output and its position state
 type State = (String, PositionState)
 
 type Coord2D = (Int, Int)
 
+-- Converts the input string to the corresponding NumPad button
 input :: String -> NumPad
 input "A" = Submit
 input n = Num (read n)
 
+-- The layout of the numeric keypad on the door
 -- >>> numpadLayout
 -- fromList [((0,1),Num 1),((0,2),Num 4),((0,3),Num 7),((1,0),Num 0),((1,1),Num 2),((1,2),Num 5),((1,3),Num 8),((2,0),Submit),((2,1),Num 3),((2,2),Num 6),((2,3),Num 9)]
 numpadLayout :: M.Map (Int, Int) NumPad
@@ -48,9 +50,11 @@ numpadLayout =
         ]
    in M.fromList [((x, y), k) | x <- [0 .. 2], y <- [0 .. 3], let key = keys !! (3 - y) !! x, isJust key, let Just k = key]
 
+-- The reverse index of the numeric keypad layout for quick lookup
 reverseIndexNumpad :: M.Map NumPad (Int, Int)
 reverseIndexNumpad = M.fromList $ map swap $ M.toList numpadLayout
 
+-- The layout of the directional keypad used to control the robot's arm
 -- >>> arrowpadLayout
 -- fromList [((0,0),LeftKey),((1,0),DownKey),((1,1),UpKey),((2,0),RightKey),((2,1),EnterKey)]
 arrowpadLayout :: M.Map (Int, Int) Key
@@ -61,9 +65,11 @@ arrowpadLayout =
         ]
    in M.fromList [((x, y), k) | x <- [0 .. 2], y <- [0 .. 1], let key = keys !! (1 - y) !! x, isJust key, let Just k = key]
 
+-- The reverse index of the directional keypad layout for quick lookup
 reverseIndexArrowpad :: M.Map Key (Int, Int)
 reverseIndexArrowpad = M.fromList $ map swap $ M.toList arrowpadLayout
 
+-- Finds the fastest sequence of key presses to type the given string on the numeric keypad
 -- >>> fastestDials Submit "029A"
 -- [[LeftKey,EnterKey,UpKey,EnterKey,RightKey,UpKey,UpKey,EnterKey,DownKey,DownKey,DownKey,EnterKey],[LeftKey,EnterKey,UpKey,EnterKey,UpKey,RightKey,UpKey,EnterKey,DownKey,DownKey,DownKey,EnterKey],[LeftKey,EnterKey,UpKey,EnterKey,UpKey,UpKey,RightKey,EnterKey,DownKey,DownKey,DownKey,EnterKey]]
 fastestDials :: NumPad -> String -> [[Key]]
@@ -72,6 +78,7 @@ fastestDials start str =
       goalState = (str, (input $ pure $ last str, []))
    in S.toList $ S.map reverse $ snd (dijkstra startState goalState M.! goalState)
 
+-- Finds the fastest sequence of key presses to move from one key to another on the directional keypad
 -- >>> fastestOptions EnterKey LeftKey
 -- [[LeftKey,DownKey,LeftKey,EnterKey],[DownKey,LeftKey,LeftKey,EnterKey]]
 fastestOptions :: Key -> Key -> [[Key]]
@@ -85,6 +92,7 @@ fastestOptions start end =
         DownKey -> ("", (Num 2, [end]))
    in S.toList $ S.map reverse $ snd (dijkstra startState endState M.! endState)
 
+-- Moves the robot's arm on the directional keypad
 move :: Coord2D -> Key -> Coord2D
 move c EnterKey = c
 move (x, y) LeftKey = (x - 1, y)
@@ -92,12 +100,15 @@ move (x, y) RightKey = (x + 1, y)
 move (x, y) UpKey = (x, y + 1)
 move (x, y) DownKey = (x, y - 1)
 
+-- Moves the robot's arm on the directional keypad and returns the new key it is pointing at
 moveArrowpad :: Key -> Key -> Key
 moveArrowpad k d = fromMaybe k (move (reverseIndexArrowpad M.! k) d `M.lookup` arrowpadLayout)
 
+-- Moves the robot's arm on the numeric keypad and returns the new key it is pointing at
 moveNumpad :: NumPad -> Key -> NumPad
 moveNumpad n d = fromMaybe n (move (reverseIndexNumpad M.! n) d `M.lookup` numpadLayout)
 
+-- Implements Dijkstra's algorithm to find the shortest path of key presses to reach the target state
 dijkstra :: State -> State -> M.Map State (Int, S.Set [Key])
 dijkstra startState (targetOutput, _) = go M.empty $ PSQ.fromList [(Nothing, Nothing, startState) :-> 0]
   where
@@ -148,27 +159,33 @@ dijkstra startState (targetOutput, _) = go M.empty $ PSQ.fromList [(Nothing, Not
               costs' = M.insert edge (cost, histories') costs
            in go costs' edges''
 
+-- Parses the input file to extract the door codes
 parseCodes :: A.Parser [String]
 parseCodes = map BSC.unpack <$> A.takeWhile1 (/= '\n') `A.sepBy` A.endOfLine
 
---- >>> loadInput "example.txt"
+-- Loads the input file and parses the door codes
+-- >>> loadInput "example.txt"
 -- ["029A","980A","179A","456A","379A"]
 loadInput :: [Char] -> IO [String]
 loadInput = (fromRight [] . A.parseOnly parseCodes <$>) . BSC.readFile . ("src/" ++)
 
+-- Extracts the numeric part of the code
 numericPart :: String -> Int
 numericPart = read . takeWhile (/= 'A')
 
+-- Calculates the sum of the complexities of the door codes for part 1
 -- >>> part1 <$> loadInput "example.txt"
 -- 126384
 part1 :: [String] -> Int
 part1 = sum . map (liftM2 (*) (countStepsWithIterations 2) numericPart)
 
+-- Calculates the sum of the complexities of the door codes for part 2
 -- >>> part2 <$> loadInput "example.txt"
 -- 154115708116294
 part2 :: [String] -> Int
 part2 = sum . map (liftM2 (*) (countStepsWithIterations 25) numericPart)
 
+-- Counts the steps required to type the code with the given number of iterations
 -- >>> map (countStepsWithIterations 25) <$> loadInput "example.txt"
 -- [82050061710,72242026390,81251039228,80786362258,77985628636]
 countStepsWithIterations :: Int -> [Char] -> Int
@@ -177,6 +194,7 @@ countStepsWithIterations n str = ST.evalState go M.empty
     go :: ST.State (M.Map ([Key], Int) Int) Int
     go = minimum <$> mapM (countStepsForDialIteration n) (fastestDials Submit str)
 
+    -- Counts the steps required for a single iteration of dialing the code
     countStepsForDialIteration :: Int -> [Key] -> ST.State (M.Map ([Key], Int) Int) Int
     countStepsForDialIteration 0 ks = return $ length ks
     countStepsForDialIteration n ks = do
